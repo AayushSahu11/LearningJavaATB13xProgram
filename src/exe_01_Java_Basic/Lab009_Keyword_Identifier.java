f1eecd56550ebfceae991392f9a097a93fb6447b
AayushSahu11 <aayushsahuofficial96@gmail.com>
2025-06-16 22:41:18 +0530

    9 June 2025 Comment, Variables, Data type

---
+++ src/exe_01_Java_Basic/Lab009_Keyword_Identifier.java
@@ -0,0 +1,11 @@
+package exe_01_Java_Basic;
+
+public class Lab009_Keyword_Identifier
+{
+//    public class static void  are called Keywords.
+//    Name of the class called Identifier.
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
